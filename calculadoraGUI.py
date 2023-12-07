4a4e8bd945557e9b1a01a6ccc0ee78f46d5b1df2
gilbertexbom <gilberto.braga0021@gmail.com>
2023-12-07 10:34:47 -0300

    - Implementação de interface gráfica.

---
+++ calculadoraGUI.py
@@ -0,0 +1,42 @@
+# Interface gráfica
+
+import PySimpleGUI as psg
+
+import calc
+
+frame_layou = [
+    [psg.Radio('Soma', 'RADIO1', key='soma', default=True)],
+    [psg.Radio('Subtração', 'RADIO1', key='sub')]
+]
+
+layout = [
+    [psg.Text('Informe N1: '), psg.InputText('', key='num1'), psg.Frame('Opções: ', frame_layou)],
+    [psg.Text('Informe N2: '), psg.InputText('', key='num2')],
+    [psg.Text('', key='total')],
+    [psg.Button('Calcular'), psg.Button('Limpar')],
+]
+
+janela = psg.Window('Calculadora do Tio', layout)
+
+while True:
+    evento, valor = janela.read()
+
+    if evento == psg.WIN_CLOSED:
+        break
+    elif evento == 'Limpar':
+        janela['num1'].update('')
+        janela['num2'].update('')
+        janela['total'].update('')
+        janela['num1'].set_focus()
+        janela['soma'].update(True)
+    else:
+        if valor['soma']:
+            v1 = int(valor['num1'])
+            v2 = int(valor['num2'])
+            total = calc.soma(v1, v2)
+            janela['total'].update('{}'.format(total))
+        else:
+            v1 = int(valor['num1'])
+            v2 = int(valor['num2'])
+            total = calc.sub(v1, v2)
+            janela['total'].update('{}'.format(total))
